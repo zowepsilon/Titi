3fd857d2fd41a63fdb6cbb77ba9f137cf061f067
Zowε <49984698+zowepsilon@users.noreply.github.com>
2026-06-11 22:30:14 +0200

    add ?stats

--- cmds/dev.py
+++ cmds/dev.py
@@ -18,7 +18,7 @@ class Developer(commands.Cog):
     @debuggable
     async def reload_names(self, ctx):
         if not self.bot.is_dev(ctx.author.id):
-            return await ctx.send("Tu dois être un développeur pour faire ça !")
+            return await ctx.send("Tu dois être une développeuse pour faire ça ! 🏳️‍⚧️")
         async with ctx.message.channel.typing():
             for m in ctx.author.guild.members:
                 self.bot.nickname_cache.set_nick(ctx.guild.id, m.id, m.display_name)
@@ -29,10 +29,25 @@ class Developer(commands.Cog):
     @debuggable
     async def debug(self, ctx):
         if not self.bot.is_dev(ctx.author.id):
-            return await ctx.send("Tu dois être un développeur pour faire ça !")
+            return await ctx.send("Tu dois être une développeuse pour faire ça ! 🏳️‍⚧️")
 
         self.bot.config["debug"] = not self.bot.config["debug"]
         await ctx.send(f"Le mode debug a été " + ("activé !" if self.bot.config["debug"] else "désactivé !"))
+
+    @commands.command()
+    @debuggable
+    async def stats(self, ctx):
+        if not self.bot.is_dev(ctx.author.id):
+            return await ctx.send("Tu dois être une développeuse pour faire ça ! 🏳️‍⚧️")
+
+        out = f"### Serveurs\n"
+        count = 0
+        for g in self.bot.guilds:
+            count += g.member_count
+            out += f"{g.name} - {g.member_count} membres\n"
+
+        out += f"Total : {self.bot.guilds} serveurs, {count} membres"
+        await ctx.send(out)
 
 def setup(bot): 
     bot.add_cog(Developer(bot))
