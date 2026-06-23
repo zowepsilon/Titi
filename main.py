3f93b7b298aaa1d61be795445afb3d60eec8442f
Zowε <49984698+zowepsilon@users.noreply.github.com>
2026-06-23 12:05:43 +0200

    fix

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from discord.ext import commands
 
 from utils import NicknameCache
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.message_content = True
 intents.moderation = True
 
